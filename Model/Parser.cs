e7b4ff5044776802a51b8f87d522a2ba8ee5c8aa
Alikku <4l1kku@gmail.com>
2024-05-17 20:19:14 +0300

    upd

---
+++ Model/Parser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourAssist.Model
+{
+    public class Parser
+    {
+        List<string> includedPeculiarities = new List<string>();
+        List<string> excludedPeculiarities = new List<string>();
+
+        void parseString(string query)
+        {
+            string[] tokens = query.Split("", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                string token = tokens[i];
+
+                bool hasPrev = i - 1 >= 0;
+                bool hasNext = i + 1 < tokens.Length;
+
+                
+            }
+        }
+    }
+}
--- Project/ViewModel/HistoryViewModel.cs
+++ Project/ViewModel/HistoryViewModel.cs
@@ -1,20 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using TourAssist.Model.Scaffold;
+using TourAssist.ViewModel.Utility;
 
 namespace TourAssist.ViewModel
 {
     public class HistoryViewModel : INotifyPropertyChanged
     {
+        string query;
+        bool searchCountry, searchRegion, searchCity;
+        ObservableCollection<Country> searchResultCountries;
+        ObservableCollection<Region> searchResultRegions;
+        ObservableCollection<City> searchResultCities;
+
+        public string Query 
+        { 
+            get
+            {
+                return query;
+            }
+            set
+            {
+                query = value;
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+        public bool SearchCountry
+        {
+            get
+            {
+                return searchCountry;
+            }
+            set
+            {
+                searchCountry = value;
+                OnPropertyChanged(nameof(SearchCountry));
+            }
+        }
+
+        public bool SearchRegion
+        {
+            get
+            {
+                return searchRegion;
+            }
+            set
+            {
+                searchRegion = value;
+                OnPropertyChanged(nameof(SearchRegion));
+            }
+        }
+
+        public bool SearchCity
+        {
+            get
+            {
+                return searchCity;
+            }
+            set
+            {
+                searchCity = value;
+                OnPropertyChanged(nameof(SearchCity));
+            }
+        }
+
+        public ObservableCollection<Country> SearchResultCountries
+        {
+            get
+            {
+                return searchResultCountries;
+            }
+            set
+            {
+                searchResultCountries = value;
+                OnPropertyChanged(nameof(SearchResultCountries));
+            }
+        }
+
+        public ObservableCollection<Region> SearchResultRegions
+        {
+            get
+            {
+                return searchResultRegions;
+            }
+            set
+            {
+                searchResultRegions = value;
+                OnPropertyChanged(nameof(SearchResultRegions));
+            }
+        }
+
+        public ObservableCollection<City> SearchResultCities
+        {
+            get
+            {
+                return searchResultCities;
+            }
+            set
+            {
+                searchResultCities = value;
+                OnPropertyChanged(nameof(SearchResultCities));
+            }
+        }
+
+        private RelayCommand? search;
+        public RelayCommand Search
+        {
+            get
+            {
+                return search ??= new RelayCommand(obj =>
+                {
+                    if (SearchCountry)
+                    {
+
+                    }
+                    if (SearchRegion)
+                    {
+
+                    }
+                    if (SearchCity)
+                    {
+
+                    }
+                });
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public PlacesSearchViewModel()
+        {
+            query = "";
         }
     }
 }
