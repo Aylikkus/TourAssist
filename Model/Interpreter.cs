2cfaf3da290745676405194a2d375baee8e55d7b
Alikku <4l1kku@gmail.com>
2024-05-25 12:14:09 +0300

    upd

---
+++ Model/Interpreter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourAssist.Model
+{
+    public enum Operator
+    {
+        AND,
+        OR
+    }
+
+    public class Node
+    {
+        public Node(Operator op, object? left, object? right)
+        {
+            Operator = op;
+            Left = left;
+            Right = right;
+        }
+
+        public Operator Operator { get; set; }
+        public object? Left { get; set; }
+        public object? Right { get; set; }
+
+
+    }
+
+    public class BinaryTree
+    {
+        public Node Head { get; private set; }
+
+        public BinaryTree(Node head)
+        {
+            Head = head;
+        }
+    }
+
+    public class Interpreter
+    {
+        private string query;
+
+        public string Query
+        {
+            get
+            {
+                return query;
+            }
+            set
+            {
+                query = value;
+            }
+        }
+
+        public BinaryTree GetAST()
+        {
+            string[] tokens = Query.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Node? prevNode = null;
+            List<Node> nodes = new List<Node>();
+            for (int i = 1; i < tokens.Length - 1; i++)
+            {
+                Operator op;
+
+                switch (tokens[i])
+                {
+                    case "и":
+                        op = Operator.AND;
+                        break;
+                    case "или":
+                        op = Operator.OR;
+                        break;
+                    default:
+                        continue;
+                }
+
+                Node node;
+
+                if (prevNode == null)
+                {
+                    node = new Node(op, tokens[i - 1], tokens[i + 1]);
+                }
+                else
+                {
+                    node = new Node(op, prevNode, tokens[i + 1]);
+                }
+
+                prevNode = node;
+                nodes.Add(node);
+            }
+
+            return new BinaryTree(nodes.Last());
+        }
+
+        public Interpreter(string query) 
+        {
+            Query = query;
+        }
+
+    }
+}
--- ViewModel/PlacesSearchViewModel.cs
+++ ViewModel/PlacesSearchViewModel.cs
@@ -1,15 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using TourAssist.Model.Scaffold;
+using TourAssist.ViewModel.Utility;
 
 namespace TourAssist.ViewModel
 {
     public class PlacesSearchViewModel : INotifyPropertyChanged
     {
+        private string query;
+
+        public string Query
+        {
+            get
+            {
+                return query;
+            }
+            set
+            {
+                query = value;
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+        private ObservableCollection<Country> searchResultCountries;
+        private ObservableCollection<Region> searchResultRegion;
+        private ObservableCollection<City> searchResultCities;
+
+        public ObservableCollection<Country> SearchResultCountries
+        {
+            get
+            {
+                return searchResultCountries;
+            }
+            set
+            {
+                searchResultCountries = value;
+                OnPropertyChanged(nameof(SearchResultCountries));
+            }
+        }
+
+        public ObservableCollection<Region> SearchResultRegion
+        {
+            get
+            {
+                return searchResultRegion;
+            }
+            set
+            {
+                searchResultRegion = value;
+                OnPropertyChanged(nameof(SearchResultRegion));
+            }
+        }
+
+        public ObservableCollection<City> SearchResultCities
+        {
+            get
+            {
+                return searchResultCities;
+            }
+            set
+            {
+                searchResultCities = value;
+                OnPropertyChanged(nameof(SearchResultCities));
+            }
+        }
+
+        private bool searchCountries;
+        private bool searchRegions;
+        private bool searchCities;
+
+        public bool SearchCountries
+        {
+            get
+            {
+                return searchCountries;
+            }
+            set
+            {
+                searchCountries = value;
+                OnPropertyChanged(nameof(SearchCountries));
+            }
+        }
+
+        public bool SearchRegions
+        {
+            get
+            {
+                return searchRegions;
+            }
+            set
+            {
+                searchRegions = value;
+                OnPropertyChanged(nameof(SearchRegions));
+            }
+        }
+
+        public bool SearchCities
+        {
+            get
+            {
+                return searchCities;
+            }
+            set
+            {
+                searchCities = value;
+                OnPropertyChanged(nameof(SearchCities));
+            }
+        }
+
+        private RelayCommand? search;
+
+        public RelayCommand Search
+        {
+            get
+            {
+                return search ??= new RelayCommand(obj =>
+                {
+
+                });
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
